95629184a7cf62b41b5521e5a1a77e732c729ee5
ikeshare192 <isaacshareef@hotmail.com>
2021-06-01 21:33:58 -0600

    Format changes

--- rtp.py
+++ rtp.py
@@ -13,15 +13,18 @@ import streamlit as st
 from io import StringIO
 import time
 
+# Image file
 st.image('INDEX2.jpg', width=700)
-#The Header
+
+# The Header
 st.markdown("<h1 style='text-align: center; color: red; '>RETRAIN PREDICTION APP</h1>" \
     , unsafe_allow_html=True)
 
 st.subheader("READ ME:")
+
 st.write("The pupose of this application is to demonstrate the value of Machine Learning \
-    used as a data analytics tool that enhaces data driven decision making.  This model will \
-    validate the process of determining the potential number of retrains a new student will \
+    used as a data analytics tool for the enhancement of data driven decision making.  This model will \
+    validate the process of determining the potential number of retrains a new student will potentially \
     have by taking in a dataframe that resembles the traits of an organization of professional \
     pilots.  The features currently include 13 professional traits and the target variable is the \
     number of retrains.  The output of this model is a single prediction of  retrains or batch \
@@ -33,6 +36,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 pickle_in = open('lr.pkl', 'rb') 
 estimator = pickle.load(pickle_in)
 
+# Creating the selectbox that enables you begin interaction
 choose_option = st.selectbox(
     "Choose a Task",
     [
@@ -44,18 +48,24 @@ choose_option = st.selectbox(
     ]
     )
 
+# Function to upload data
 def upload_file():
     uploaded_file = st.file_uploader("", type = "csv")
+    
     if uploaded_file is None:
         st.stop()
+    
     if uploaded_file is not None:
         df=pd.read_csv(uploaded_file)
         df = pd.DataFrame(df)
+    
     return df
 
+# Function to create bar chart for uploaded data
 def bar_chart(df):
     x=np.arange(len(df.columns))
     col_sums = []
+    
     for column in df.columns:
         sums = sum(df[column])
         col_sums.append(sums)
@@ -71,19 +81,10 @@ def bar_chart(df):
     plt.xlabel('Features')
     plt.ylabel('Model Attribute(sums)')
     plt.title("Pilot Attributes Bar Chart", fontsize=16)
+    
     return fig
 
-@st.cache
-def ml(df):
-    y=df["# of Retrains"]
-    X = df.drop("# of Retrains", axis=1)
-    X_train, X_test, y_train, y_test = train_test_split(
-        X,y, random_state=42, test_size = 0.2)
-    estimator = gbr()
-    estimator.fit(X_train, y_train)
-    return estimator
-
-#creates a sidebar slider
+# Creates a sidebar for slider data input
 def get_user_input():
     value = []
     names = [
@@ -101,16 +102,17 @@ def get_user_input():
         'Prev Capt',
         'Prev Rot-Wing'
     ]
+
     dictionary = {}
 
-    #iteration to create the sliders
+    # Loop to create the sliders used for data input
     for name in names:
         minimum = 0 #as item because of the int32, int mismatch
         maximum = 1
         sliders = st.sidebar.slider(str(name), minimum, maximum, 1)
         value.append(sliders)
 
-    #return value
+    # Return slider values as a dictionary then a dataframe
     dictionary = dict(zip(names, value))
     features = pd.DataFrame(dictionary, index=[0])
     return features
@@ -127,9 +129,6 @@ def main():
         st.pyplot(fig)
 
     if choose_option == "3. Make Individual Predictions":
-        #df=upload_file()
-        #estimator = ml(df)
-        #features = get_user_input(df.drop(columns = "# of Retrains"))
         features = get_user_input()
         st.write(features)
         output = np.round(estimator.predict(features))
